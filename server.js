27a44d6f56577d5d018e18b54ab54e6456e772f0
elianamjreige1-svg <eliana.m.jreige1@gmail.com>
2026-05-13 17:54:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(express.static("public"));
-require("dotenv").config();
+
 // ================= DB =================
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
